d1b2204c7491e35567e01d536dd7b031b302a5ec
Alessandra Soares dos Santos <alessandra.ss@edu.sc.senai.br>
2023-05-30 20:36:47 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,46 @@
+﻿using DemoFilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoFilmesApi.Controllers
+{
+    [Route("api/filmes")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        // GET: api/<FilmesController>
+        [HttpGet]
+        public IEnumerable<Filme> Get()
+        {
+            return MockFilmes.Filmes;
+        }
+
+        // GET api/<FilmesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FilmesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FilmesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FilmesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
